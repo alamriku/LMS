1636205222d2c88ea872838e02dd4bf5f5fac56b
Riku <alamriku069@gmail.com>
2020-10-29 23:12:19 +0600

    loan_requets migration done

---
+++ database/migrations/2020_10_29_170920_create_loan_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoanRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('status');
+            $table->integer('status_changed_by');
+            $table->string('reason');
+            $table->timestamp('status_change_date');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan_requests');
+    }
+}
